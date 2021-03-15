9ac8086edb1f73646d2ba5099cc08ad0f0b1e92f
Robert MacDavid <robert@opennetworking.org>
2021-03-15 13:16:35 -0400

    fixed p4 entity names

--- app/src/main/java/org/princeton/conquest/Constants.java
+++ app/src/main/java/org/princeton/conquest/Constants.java
@@ -27,16 +27,16 @@ public class Constants {
     public static int QUEUE_DELAY_RANGE_MAX = (1 << QUEUE_DELAY_BIT_WIDTH) - 1;
 
     // P4 Entities
-    public static PiTableId REPORT_TRIGGER_TABLE = PiTableId.of("FabricEgress.conquest.tb_per_flow_action");
+    public static PiTableId REPORT_TRIGGER_TABLE = PiTableId.of("FabricEgress.conquest_egress.tb_per_flow_action");
 
     public static PiMatchFieldId FLOW_SIZE_IN_QUEUE = PiMatchFieldId.of("snap_0");
     public static PiMatchFieldId QUEUE_DELAY = PiMatchFieldId.of("q_delay");
     public static PiMatchFieldId RANDOM_BITS = PiMatchFieldId.of("random_bits");
     public static PiMatchFieldId ECN_BITS = PiMatchFieldId.of("ecn");
 
-    public static PiActionId NOP = PiActionId.of("FabricEgress.conquest.conq_nop");
-    public static PiActionId DROP = PiActionId.of("FabricEgress.conquest.drop");
-    public static PiActionId MARK_ECN = PiActionId.of("FabricEgress.conquest.mark_ECN");
-    public static PiActionId TRIGGER_REPORT = PiActionId.of("FabricEgress.conquest.trigger_report");
-    public static PiActionId NOT_TRIGGER_REPORT = PiActionId.of("FabricEgress.conquest.not_trigger_report");
+    public static PiActionId NOP = PiActionId.of("FabricEgress.conquest_egress.conq_nop");
+    public static PiActionId DROP = PiActionId.of("FabricEgress.conquest_egress.drop");
+    public static PiActionId MARK_ECN = PiActionId.of("FabricEgress.conquest_egress.mark_ECN");
+    public static PiActionId TRIGGER_REPORT = PiActionId.of("FabricEgress.conquest_egress.trigger_report");
+    public static PiActionId NOT_TRIGGER_REPORT = PiActionId.of("FabricEgress.conquest_egress.not_trigger_report");
 }
